fb8df0eb03ac2c5836a1f2a409e674f5d62d40ba
graceup <921443349@qq.com>
2017-12-27 15:08:45 +0800

    add deploy message

--- src/main/webapp/assets/js/deploy.js
+++ src/main/webapp/assets/js/deploy.js
@@ -5,12 +5,35 @@ $(function() {
 	/*
 	 */
 	 $('#submit-button').on('click', function() {
+		 
 	
-		 $('#deploy-form').ajaxForm(function() {
+		 $('#deploy-form').ajaxForm({
+				 beforeSubmit: function(formData, jqForm, options){
+					 console.log(formData);
+					 var fileValue=$('#doc-form-file').val();
+					 
+					 if(fileValue==''){
+						 alert('请选择war包');
+						 return false;
+					 }
+					 var fileType = fileValue.substr(fileValue.indexOf("."));
+					 if(fileType!='.war'){
+						 alert('请选择war包');
+						 return false;
+					 }
+					 
+				 },
+				 success:function(data) {
+					 console.log(data);
+					var html = $.parseHTML(data);
+//					console.log(html);
+					var msg=html[13].childNodes[1].childNodes[0].childNodes[3].childNodes[0].childNodes[0].textContent;
+					
+					console.log(msg);
+					
+					alert(msg);
 	         
-			 alert("Thank you for your comment!");
-	         
-	     });
+	     }});
 	
 	 });
 
